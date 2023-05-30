03abc702a7b047d3dba8677425729e5b2d12131f
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-05-30 12:44:30 -0400

    added pokemon list iteration and styling

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 let pokemonList = [
   {
-    name: Bulbasaur,
+    name: "Bulbasaur",
     height: 0.7,
-    type: [grass, poison],
+    type: ["grass", "poison"],
   },
   {
-    name: Ivyasaur,
+    name: "Ivyasaur",
     height: 1,
-    type: [grass, poison],
+    type: ["grass", "poison"],
   },
   {
-    name: Venuasaur,
+    name: "Venuasaur",
     height: 2,
-    type: [grass, poison],
+    type: ["grass", "poison"],
   },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemon = `<p class="pokemon">
+${pokemonList[i].name} (height: ${pokemonList[i].height})
+</p>`;
+
+  let tallestPokemon = `<p class="pokemon">
+${pokemonList[i].name} (height: ${pokemonList[i].height})- <span class="pokemon__size">Wow, that's big</span>
+</p>`;
+
+  pokemonList[i].height < 2
+    ? document.write(pokemon)
+    : document.write(tallestPokemon);
+}
