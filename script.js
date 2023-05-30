fe2912a13d78cd378f815ede7de5f3926896019f
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-05-29 21:48:46 -0400

    remove e1.1 and added pokemon list for e1.2

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-alert("Hello World");
-let favFood = "Rice";
-document.write(favFood);
+let pokemonList = [
+  {
+    name: Bulbasaur,
+    height: 0.7,
+    type: [grass, poison],
+  },
+  {
+    name: Ivyasaur,
+    height: 1,
+    type: [grass, poison],
+  },
+  {
+    name: Venuasaur,
+    height: 2,
+    type: [grass, poison],
+  },
+];
