2fbc3386f7cd7d8b4361eac91d92ad2c2d0c7514
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-06-02 23:15:14 -0400

    added IIFE, validtaions, and search

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    type: ["grass", "poison"],
-  },
-  {
-    name: "Ivyasaur",
-    height: 1,
-    type: ["grass", "poison"],
-  },
-  {
-    name: "Venuasaur",
-    height: 2,
-    type: ["grass", "poison"],
-  },
-];
+// Validate a pokemon if it is and object and keys match with the format.
 
-for (let i = 0; i < pokemonList.length; i++) {
+const validatePokemon = (pokemon) => {
+  //Check if the pokemon is object
+
+  if (
+    typeof pokemon === "object" &&
+    !Array.isArray(pokemon) &&
+    pokemon !== null
+  ) {
+    // Check if the keys are the same as the format
+
+    let = ["name", "height", "width"];
+    return format.every((val, i) => val === Object.keys(pokemon)[i]);
+  }
+  return false;
+};
+
+let pokemonRepository = (() => {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      type: ["grass", "poison"],
+    },
+    {
+      name: "Ivyasaur",
+      height: 1,
+      type: ["grass", "poison"],
+    },
+    {
+      name: "Venuasaur",
+      height: 2,
+      type: ["grass", "poison"],
+    },
+  ];
+
+  const getAll = function () {
+    return pokemonList;
+  };
+
+  const add = function (item) {
+    if (validatePokemon(item)) pokemonList.push(item);
+  };
+
+  const search = function (searchText) {
+    return pokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  };
+
+  return {
+    getAll,
+    add,
+    search,
+  };
+})();
+
+function createPokemon(item) {
   let pokemon = `<p class="pokemon">
-${pokemonList[i].name} (height: ${pokemonList[i].height})
-</p>`;
-
+  ${item.name} (height: ${item.height})
+  </p>`;
   let tallestPokemon = `<p class="pokemon">
-${pokemonList[i].name} (height: ${pokemonList[i].height})- <span class="pokemon__size">Wow, that's big</span>
+${item.name} (height: ${item.height})- <span class="pokemon__size">Wow, that's big</span>
 </p>`;
-
-  pokemonList[i].height < 2
-    ? document.write(pokemon)
-    : document.write(tallestPokemon);
+  item.height < 2 ? document.write(pokemon) : document.write(tallestPokemon);
 }
+
+pokemonRepository.getAll().forEach(createPokemon);
